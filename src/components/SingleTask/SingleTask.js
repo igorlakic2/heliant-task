4be25a1637ba082befedef3351352cab74145843
igor.lakic2 <igor.lakic2@gmail.com>
2022-11-07 13:58:06 +0100

    single task card done

--- src/components/SingleTask/SingleTask.js
+++ src/components/SingleTask/SingleTask.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./SingleTask.css";
 
-const SingleTask = () => {
-  return <div className="task-component">SingleTask</div>;
+const SingleTask = ({ title, priority, onClick }) => {
+  return (
+    <div className="task-component" onClick={onClick}>
+      <p>Naziv: {title}</p>
+      <p>Prioritet: {priority}</p>
+      <div>
+        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+        <label htmlFor="vehicle1">Završen</label>
+      </div>
+      <button>Obriši</button>
+    </div>
+  );
 };
 
 export default SingleTask;
--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -15,7 +15,12 @@ const Tasks = () => {
       </button>
       <div className="tasks-div">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((task, id) => (
-          <SingleTask key={id} />
+          <SingleTask
+            key={id}
+            title={`Naslov ${task}`}
+            priority="Visok"
+            onClick={() => console.log("Single")}
+          />
         ))}
       </div>
     </div>
