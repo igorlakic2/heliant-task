9cd5992e5a8c0e1446dad1d6dabb553c943a0ec1
igor.lakic2 <igor.lakic2@gmail.com>
2022-11-07 13:38:15 +0100

    tasks page - added button and single task card

--- src/pages/Tasks/Tasks.js
+++ src/pages/Tasks/Tasks.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import SingleTask from "../../components/SingleTask/SingleTask";
 import "./Tasks.css";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([]);
+
   return (
-    <div className="tasks-div">
-      {[1, 2, 3, 4, 5].map((task) => (
-        <SingleTask />
-      ))}
+    <div>
+      <button
+        className="add-button"
+        onClick={() => console.log("Dodaj zadatak")}
+      >
+        Dodaj zadatak
+      </button>
+      <div className="tasks-div">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((task, id) => (
+          <SingleTask key={id} />
+        ))}
+      </div>
     </div>
   );
 };
